67e256c1683be2b8413f74931375aefeb9e6bfe4
Jessica Janiuk <jessica.janiuk@gmail.com>
2017-01-15 18:46:27 -0500

    added page controller and migrations for pages

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,5 +1,6 @@
 var Router = require('restify-router').Router;
 var postController = require('../controllers/postController');
+var pageController = require('../controllers/pageController');
 
 var routerInstance = new Router();
 
@@ -9,6 +10,13 @@ routerInstance.get('/post/:id', postController.getById);
 routerInstance.post('/post',postController.add);
 routerInstance.put('/post/:id',postController.update);
 routerInstance.del('/post/:id',postController.delete);
+
+// page methods
+routerInstance.get('/page', pageController.getAll);
+routerInstance.get('/page/:id', pageController.getById);
+routerInstance.post('/page',pageController.add);
+routerInstance.put('/page/:id',pageController.update);
+routerInstance.del('/page/:id',pageController.delete);
 
 // add a route like you would on a restify server instance 
 routerInstance.get('/ping', function (req, res, next) {
