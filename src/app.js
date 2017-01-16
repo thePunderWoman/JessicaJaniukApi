9001096e259049308c090a3826d6854baa78e091
Jessica Janiuk <jessica.janiuk@gmail.com>
2017-01-16 10:01:24 -0500

    lots of progress

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ var restify = require('restify');
 var restifyValidator = require('restify-validator');
 var routes = require('./config/routes');
 
-var server = restify.createServer();
+var server = restify.createServer({
+  name: 'JessicaJaniuk v1'
+});
 
 server.pre(restify.CORS({
   origins: ['https://jessicajaniuk.com', 'http://localhost:4200'],   // defaults to ['*']
 }));
+server.use(restify.authorizationParser());
 server.use(restify.bodyParser());
 server.use(restify.queryParser());
 server.use(restify.gzipResponse());
